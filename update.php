cbfa7b4c937e46cb8320a111358305a37b565e81
ade-ryzki <ade.ryzki@gmail.com>
2023-01-26 14:15:10 +0700

    update

---
+++ update.php
@@ -0,0 +1,32 @@
+<?php
+require("koneksi.php");
+
+$response = array();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //post data 
+    $id = $_POST["id"];
+    $nama = $_POST["nama"];
+    $ktp = $_POST["ktp"];
+    $alamat = $_POST["alamat"];
+    $telepon = $_POST["telepon"];
+
+    $perintah = "UPDATE iptv SET nama ='$nama', ktp ='$ktp', alamat ='$alamat', telepon ='$telepon' WHERE id = '$id'";
+    $eksekusi = mysqli_query($konek, $perintah);
+    $cek = mysqli_affected_rows($konek);
+
+    if($cek > 0){
+        $response["kode"] = 1;
+        $response["pesan"] = "Data Berhasil Update";
+        
+    }else{
+        $response["kode"] = 0;
+        $response["pesan"] = "Data Gagal Update";
+    }
+}else{
+    $response["kode"] = 0;
+    $response["pesan"] = "Tidak Ada Post Data";
+}
+
+echo json_encode($response);
+mysqli_close($konek);
